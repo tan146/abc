c5a3e98e570f042b4d221575a62099e42c68a536
tanzongren <2470731702@qq.com>
2020-11-15 20:23:54 +0800

    更新2

--- src/main/java/com/baosight/test.java
+++ src/main/java/com/baosight/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello,world");
         System.out.println("更新1");
+        System.out.println("更新2");
 
     }
 }
